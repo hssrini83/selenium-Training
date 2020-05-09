9558bb2ba27538acf01a02e9a8b1ff406db0d60c
hssrini83 <srinivasa.rangan@in.ibm.com>
2020-05-09 00:46:55 -0400

    frameworkcode push

--- src/com/training/pom/LoginPOM.java
+++ src/com/training/pom/LoginPOM.java
@@ -2,6 +2,8 @@ package com.training.pom;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -16,11 +18,84 @@ public class LoginPOM {
 	@FindBy(id="login")
 	private WebElement userName; 
 	
+	
+	
 	@FindBy(id="password")
 	private WebElement password;
 	
 	@FindBy(id="formLogin_submitAuth")
 	private WebElement loginBtn; 
+	
+	/*
+	 * // Test case RETC_010 "New Launch"
+	 */	
+	@FindBy(xpath="//a[contains(text(),'New Launch')]")
+    private WebElement New_Launch;
+	
+	@FindBy(xpath="//div[@id='wpmm-megamenu']//div[@class='wpmm-post post-4505']//a[contains(text(),'prestige')]")
+    private WebElement prestige1;
+	
+	@FindBy(xpath="//input[@name='your-name']")
+	public WebElement Your_Name;
+	
+	@FindBy(xpath="//input[@name='your-email']")
+	public WebElement Email;
+	
+	@FindBy(xpath="//input[@name='your-subject']")
+	public WebElement subject;
+	
+	@FindBy(xpath="//textarea[@name='your-message']")
+	public WebElement your_message;
+	
+	@FindBy(xpath="//input[@class='wpcf7-form-control wpcf7-submit']")
+	public WebElement Send;
+	
+	@FindBy(xpath="//div[@class='wpcf7-response-output wpcf7-display-none wpcf7-mail-sent-ng']")
+	public WebElement ErrorSendingyourmessage;
+	
+	/*
+	 * // Test case RETC_011 "LOG IN/REGISTER button"
+	 */	
+	@FindBy(xpath="//a[@class='sign-in']")
+	public WebElement Login_Register;
+	
+	@FindBy(xpath="//input[@id='user_login']")
+	public WebElement Username;
+	
+	@FindBy(xpath="//input[@id='user_pass']")
+	public WebElement Password;
+	
+	@FindBy(xpath="//input[@name='login']")
+	public WebElement Sign_In;
+	
+	@FindBy(xpath="//div[contains(text(),'Dashboard')]")
+	public WebElement AdminDashboard;
+	
+	@FindBy(xpath="//img[contains(@class,'avatar avatar-26 photo')]")
+	public WebElement OptionLogout;
+	
+	@FindBy(xpath="//a[contains(@class,'ab-item')][contains(text(),'Log Out')]")
+	public WebElement Logout;
+	
+	/*
+	 * // Test case RETC-012 "LOG IN/REGISTER button"
+	 */
+	
+	@FindBy(xpath="//a[contains(text(),'Lost Your Password?')]")
+	public WebElement Lostyourpassword;
+	
+	@FindBy(xpath="//input[@id='user_login']")
+	public WebElement Email_Address_Textbox;
+	
+	@FindBy(xpath="//input[@name='submit']")
+	public WebElement ResetPassword;
+	
+	@FindBy(xpath="//p[contains(text(),'The email could not be sent.')]")
+	public WebElement ErrorMessage;
+	
+	/*
+	 * // End of variable declaration for test cases"
+	 */
 	
 	public void sendUserName(String userName) {
 		this.userName.clear();
@@ -34,5 +109,34 @@ public class LoginPOM {
 	
 	public void clickLoginBtn() {
 		this.loginBtn.click(); 
+	}
+	
+	public void clickNew_Launch() {
+		//this.New_Launch.;
+		Actions builder = new Actions(driver);
+		Action hover= builder.moveToElement(this.New_Launch).build();
+		hover.perform();
+		
+	}
+	public void clickBtn(WebElement S1) {
+		S1.click();
+	}
+	public void click_prestige() {
+		this.prestige1.click();
+	}
+	
+	public void inputtext(WebElement I,String S) {
+		I.sendKeys(S);
+	}
+	
+	public void messgaeValidate(WebElement W, String S) {
+		if (W.isDisplayed()) {
+			System.out.println(S+ " displayed");
+		}
+			else {
+				System.err.println(S+ "is not displayed");
+				
+		
+		}
 	}
 }
--- tests/com/training/sanity/tests/LoginTests.java
+++ tests/com/training/sanity/tests/LoginTests.java
@@ -45,11 +45,59 @@ public class LoginTests {
 		Thread.sleep(1000);
 		driver.quit();
 	}
-	@Test
+	@Test(priority=1, enabled=true)
 	public void validLoginTest() {
-		loginPOM.sendUserName("admin");
-		loginPOM.sendPassword("admin@123");
-		loginPOM.clickLoginBtn(); 
-		screenShot.captureScreenShot("First");
+		/*
+Test case name =RETC_011
+* 1. Click on New Launch tab
+Result:application should display the new launches
+2. Click on prestige (http://realty-real-estate.upskills.in/prestige-33/)
+Result:: Right side down "For Enquires" Should get displayed
+3. Enter valid credentials in Your Name textbox
+Result::Entered credentials in Your Name textbox should get displayed
+4. Enter valid credentials in Your Email textbox
+Result::Entered credentials in Your Email textbox should get displayed
+5. Enter valid credentials in Subject textbox
+Result:: Entered credentials in Subject textbox should get displayed
+6. Enter valid credentials in Your Message textbox
+Result::Entered credentials in Your Message textbox should get displayed
+7. Click on Send button
+Result::There was an error trying to send your message. Please try again later should be displayed.
+		 */
+		
+		/*
+		 * loginPOM.sendUserName("admin"); loginPOM.sendPassword("admin@123");
+		 * loginPOM.clickLoginBtn(); screenShot.captureScreenShot("First");
+		 * loginPOM.createDescription();
+		 */
+		loginPOM.clickNew_Launch();
+		loginPOM.click_prestige();
+		loginPOM.inputtext(loginPOM.Your_Name, "manzoor");
+		loginPOM.inputtext(loginPOM.Email, "manzoor@gmail.com");
+		loginPOM.inputtext(loginPOM.subject, "apartments");
+		loginPOM.inputtext(loginPOM.your_message, "looking for an apartments");
+		loginPOM.clickBtn(loginPOM.Send);
+		loginPOM.messgaeValidate(loginPOM.ErrorSendingyourmessage,"Message");
+	}
+	
+	@Test(priority=2, enabled=true)
+	public void loginApp() {
+		loginPOM.clickBtn(loginPOM.Login_Register);
+		loginPOM.inputtext(loginPOM.Username, "manzoor@gmail.com");
+		loginPOM.inputtext(loginPOM.Password,"manzoor");
+		loginPOM.clickBtn(loginPOM.Sign_In);
+		loginPOM.messgaeValidate(loginPOM.AdminDashboard,"Dashboard");
+	}
+	
+	@Test(priority=3, enabled=true)
+	public void forgotPass() {
+		loginPOM.clickBtn(loginPOM.Login_Register);
+		loginPOM.clickBtn(loginPOM.Lostyourpassword);
+		loginPOM.inputtext(loginPOM.Email_Address_Textbox, "manzoor@gmail.com");
+		loginPOM.clickBtn(loginPOM.ResetPassword);
+		loginPOM.messgaeValidate(loginPOM.ErrorMessage, "Error Message");
+		
 	}
 }
+
+
